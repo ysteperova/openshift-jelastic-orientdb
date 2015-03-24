160cdac3043e068e567ecdff9a6249fb2b0b995c
SiryjVyiko <dz@hivext.net>
2015-03-24 23:10:55 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -12,7 +12,11 @@ J_OPENSHIFT_APP_ADM_USER="root";
 
 function _setPassword() {
     ORIENTDB_SERVER_PROP_FILE="${OPENSHIFT_ORIENTDB_DIR}/versions/2/config/orientdb-server-config.xml";
-   
+    service cartridge stop >> /dev/null 2>&1; 
+    sed -i '/<user name=.*password=/d' $ORIENTDB_SERVER_PROP_FILE;
+    sed -i "/<users>/a <user name=\"guest\" password=\"$J_OPENSHIFT_APP_ADM_PASSWORD\" resources=\"connect,server.listDatabases,server.dblist\"\/>" $ORIENTDB_SERVER_PROP_FILE;
+    sed -i "/<users>/a <user name=\"root\" password=\"$J_OPENSHIFT_APP_ADM_PASSWORD\" resources=\"*\"\/>" $ORIENTDB_SERVER_PROP_FILE;
+    service cartridge start >> /dev/null 2>&1; 
 }
 
 
